861f88919e1ab617a5cb6038c65341ee211ad81c
zdbx13 <mllurba13@gmail.com>
2023-02-22 16:48:55 +0100

    Service folder

--- server/index.js
+++ server/index.js
@@ -14,8 +14,13 @@ const mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     database: 'CineLike',
+<<<<<<< HEAD
     user: 'root',
     password: '1234'
+=======
+    user: 'admin',
+    password: 'admin'
+>>>>>>> 596435c (Service folder)
 });
 //fem servir la BBDD que tenim
 app.get('/db', function (req, res) {
